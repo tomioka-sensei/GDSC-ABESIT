8cae4e7139c7dd31c1208d32c83252474e792de2
Ishaan2053 <Ishaan2053@gmail.com>
2023-10-31 23:47:31 +0530

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import { Inter } from 'next/font/google'
-import Hero from '@/components/Hero'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import Contact from '@/components/Contact'
-import Image from 'next/image'
-import image2 from '@/assets/image2.png'
+import Hero from '../components/Hero'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Contact from '../components/Contact'
 import Head from 'next/head'
-import PreviousEvents from '@/components/PreviousEvents'
-import About from '@/components/About'
-import ProgressBar from '@/components/ProgressBar'
+import PreviousEvents from '../components/PreviousEvents'
+import About from '../components/About'
+import ProgressBar from '../components/ProgressBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,13 +17,12 @@ export default function Home() {
         <title>GDSC ABESIT</title>
       </Head>
       <ProgressBar />
-      <main className={`flex flex-col bg-gradient-to-tr from-slate-100 to-stone-100 items-center justify-between ${inter.className}`}>
+      <main className={`flex flex-col overflow-hidden bg-gradient-to-tr from-neutral-100 to-stone-100 items-center justify-between ${inter.className}`}>
         <Navbar />
         <Hero />
         {/* Other components */}
         <About />
         <PreviousEvents />
-        <Image src={image2} />
         <Contact />
         <Footer />
       </main>
