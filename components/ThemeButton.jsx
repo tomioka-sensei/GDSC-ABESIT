c85ee1a68cc79243c93a59e938bbcf9cb98517b5
Ishaan2053 <Ishaan2053@gmail.com>
2023-11-09 11:02:46 +0530

    updated theme styles

--- components/ThemeButton.jsx
+++ components/ThemeButton.jsx
@@ -16,9 +16,9 @@ const ThemeButton = () => {
       >
        
         {currentTheme === "light" ? (
-          <MoonIcon className="text-white shadow-2xl h-9 w-9 bg-neutral-700 transition-all hover:text-gray-300 border-2 p-1 rounded-full hover:scale-105" />
+          <MoonIcon className="text-white hover:shadow-lg h-9 w-9 bg-neutral-500 transition-all hover:text-gray-200 border-2 p-1 rounded-full hover:scale-105" />
         ) : (
-          <SunIcon className=" dark:text-white shadow-2xl h-9 w-9 bg-neutral-700 transition-all dark:hover:text-yellow-500 border-2 p-1 rounded-full hover:scale-105" />
+          <SunIcon className=" dark:text-white hover:shadow-2xl h-9 w-9 bg-neutral-700 transition-all dark:hover:text-yellow-500 border-2 p-1 rounded-full hover:scale-105" />
         )}
       </button>
     </div>
