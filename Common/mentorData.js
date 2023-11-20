139e23331f6301efb2cf33212c486c45e106790b
keshav <kmalik0907@gmail.com>
2023-11-20 18:24:52 +0530

    Updated Data

--- Common/mentorData.js
+++ Common/mentorData.js
@@ -1,6 +1,6 @@
-export const  mentorData = [
+export const mentorData = [
     {
-        id:1,
+        id: 1,
         name: "Devraj Rathi",
         image: "https://res.cloudinary.com/dkgwk8ey5/image/upload/v1699609759/Screenshot_2023-11-10_at_3.18.31_PM_vmagbm.png",
         linkedin: "https://www.linkedin.com/in/iamdevraj/",
@@ -8,7 +8,7 @@ export const  mentorData = [
         occuparion: "SDE-2 @GoComet"
     },
     {
-        id:2,
+        id: 2,
         name: "Kumar Satyarth",
         image: "https://res.cloudinary.com/dkgwk8ey5/image/upload/v1699611004/Screenshot_2023-11-10_at_3.39.39_PM_kudmko.png",
         linkedin: "https://www.linkedin.com/in/ksatyarth2/",
@@ -16,7 +16,7 @@ export const  mentorData = [
         occuparion: "Smart Contract Developer @Timeswap"
     },
     {
-        id:3,
+        id: 3,
         name: "Hari Om Kushwaha",
         image: "https://res.cloudinary.com/dkgwk8ey5/image/upload/v1699611866/Screenshot_2023-11-10_at_3.53.50_PM_yzrngx.png",
         linkedin: "https://www.linkedin.com/in/hari-om-kushwaha-6a698a192/",
@@ -24,7 +24,7 @@ export const  mentorData = [
         occuparion: "SWE @ Navyug Infosolutions Pvt.Ltd"
     },
     {
-        id:3,
+        id: 3,
         name: "Tarun aggarwal",
         image: "https://res.cloudinary.com/dkgwk8ey5/image/upload/v1699631814/Screenshot_2023-11-10_at_9.25.23_PM_qiaiay.png",
         linkedin: "https://www.linkedin.com/in/ertarunagarwal/",
@@ -32,7 +32,7 @@ export const  mentorData = [
         occuparion: "SDE-2 @GoComet"
     },
     {
-        id:4,
+        id: 4,
         name: "Adya Pandey",
         image: "https://res.cloudinary.com/dkgwk8ey5/image/upload/v1699611626/adya_zory4i.jpg",
         linkedin: "https://linkedin.com/in/adya-pandey-593393170/",
@@ -40,7 +40,7 @@ export const  mentorData = [
         occuparion: "Google DSC Lead '21"
     },
     {
-        id:5,
+        id: 5,
         name: "Pari Tomar",
         image: "https://res.cloudinary.com/dkgwk8ey5/image/upload/v1699611430/pari_qouven.jpg",
         linkedin: "https://www.linkedin.com/in/tomarpari90/",
@@ -48,7 +48,7 @@ export const  mentorData = [
         occuparion: "Junior Blockchain Developer @Nest"
     },
     {
-        id:6,
+        id: 6,
         name: "Prakhar Sharma",
         image: "https://res.cloudinary.com/dkgwk8ey5/image/upload/v1699611428/prakhar_hhpkhs.jpg",
         linkedin: "https://www.linkedin.com/in/prakhar-sharma888/",
@@ -56,7 +56,7 @@ export const  mentorData = [
         occuparion: "Associate Blockchain Developer @Nest"
     },
     {
-        id:7,
+        id: 7,
         name: "Aryash Chaudhary",
         image: "https://res.cloudinary.com/dkgwk8ey5/image/upload/v1699614960/Screenshot_2023-11-10_at_4.42.58_PM_exgjfo.png",
         linkedin: "https://www.linkedin.com/in/aryash19/",
@@ -64,7 +64,15 @@ export const  mentorData = [
         occuparion: "MERN Developer"
     },
     {
-        id:7,
+        id: 8,
+        name: "Shivam Kumar",
+        image: "https://res.cloudinary.com/dkgwk8ey5/image/upload/v1700484382/SAVE_20231116_092421_v91hfs.jpg",
+        linkedin: "https://www.linkedin.com/in/shivam-kumar-1ab28a212/",
+        instagram: "https://www.instagram.com/connect_shivam_here/",
+        occuparion: "Google DSC Lead '22"
+    },
+    {
+        id: 9,
         name: "Shubham Kumar",
         image: "https://res.cloudinary.com/dkgwk8ey5/image/upload/v1699614952/shubham_knins6.jpg",
         linkedin: "https://www.linkedin.com/in/shubham032/",
@@ -72,3 +80,4 @@ export const  mentorData = [
         occuparion: "Community Lead @EdFoal "
     },
 ]
+
